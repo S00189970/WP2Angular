54a106c148f41ed370c7a321118be39e64dbe1bf
Paul Sherlock <S00189970@mail.itsligo.ie>
2021-11-03 16:37:14 +0000

    first commit

---
+++ src/app/book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+    _id: string,
+    title: string,
+    year_written: number,
+    edition: string,
+    price: number,
+    author : { name : string, nationality : string},
+    tags: string[];
+
+}
+//ng serve
